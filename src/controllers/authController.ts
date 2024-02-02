c26e669173c326ed861e04e7ee2a498f33da4317
Henrique Costa Cereja <cerejahenrique@gmail.com>
2024-02-02 18:04:13 -0300

    Backend: Rota de login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { userService } from "../services/userServices";
+import { jwtService } from "../services/jwtServices";
 
 export const authController = {
   register: async (req: Request, res: Response) => {
@@ -23,6 +24,46 @@ export const authController = {
       });
 
       return res.status(201).json(newUser);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  login: async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await userService.findByEmail(email);
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "user not found/incorrect password" });
+      }
+
+      user.checkPassword(password, (err, isSame) => {
+        if (err) {
+          return res.status(400).json({ message: err.message });
+        }
+
+        if (!isSame) {
+          return res
+            .status(401)
+            .json({ message: "user not found/incorrect password" });
+        }
+
+        const payload = {
+          id: user.id,
+          firstName: user.firstName,
+          email: user.email,
+        };
+
+        const token = jwtService.signToken(payload, "1d");
+
+        return res.json({ authenticated: true, token: token });
+      });
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message });
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import { authController } from "../controllers/authController";
 const userRouter = express.Router();
 
 userRouter.post("/auth/register", authController.register);
+userRouter.post("/auth/login", authController.login);
 
 export { userRouter };
