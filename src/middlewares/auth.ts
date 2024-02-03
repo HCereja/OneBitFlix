a1be678d0bb8c7cfc2d3b6e1258f577c48718aed
Henrique Costa Cereja <cerejahenrique@gmail.com>
2024-02-03 11:35:32 -0300

    Backend: Middleware de autenticação da rota de reprodução dos videos

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -21,14 +21,41 @@ export function ensureAuth(
 
   const token = authorizationHeader.replace(/Bearer /, "");
 
-  jwtService.verifyToken(token, (err, decoded) => {
+  jwtService.verifyToken(token, async (err, decoded) => {
     if (err || typeof decoded === "undefined") {
       return res.status(401).json({ message: "unauthorized: invalid token" });
     }
 
-    userService.findByEmail((decoded as JwtPayload).email).then((user) => {
-      req.user = user;
-      next();
-    });
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user;
+
+    next();
+  });
+}
+
+export function ensureAuthViaQuery(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const { token } = req.query;
+
+  if (!token) {
+    return res.status(401).json({ message: "unauthorized: token not found" });
+  }
+
+  if (typeof token !== "string") {
+    return res.status(400).json({ message: "Token must be of type string" });
+  }
+
+  jwtService.verifyToken(token, async (err, decoded) => {
+    if (err || typeof decoded === "undefined") {
+      return res.status(401).json({ message: "unauthorized: invalid token" });
+    }
+
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user;
+
+    next();
   });
 }
--- src/routes/episodeRoutes.ts
+++ src/routes/episodeRoutes.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import { episodesController } from "../controllers/episodeController";
+import { ensureAuthViaQuery } from "../middlewares/auth";
 
 const episodeRouter = express.Router();
 
-episodeRouter.get("/episodes/stream", episodesController.stream);
+episodeRouter.get(
+  "/episodes/stream",
+  ensureAuthViaQuery,
+  episodesController.stream
+);
 
 export { episodeRouter };
