b1d1f09e96aff2591c9bebf3fca10ac2c135f7c4
Henrique Costa Cereja <cerejahenrique@gmail.com>
2024-02-04 15:13:00 -0300

    Backend: Rota de detalhes do usuário logado

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,6 +3,18 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userServices";
 
 export const userController = {
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    const user = req.user!;
+
+    try {
+      return res.json(user);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
   watching: async (req: AuthenticatedRequest, res: Response) => {
     const userId = req.user!.id;
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,5 +8,6 @@ const userRouter = express.Router();
 userRouter.post("/auth/register", authController.register);
 userRouter.post("/auth/login", authController.login);
 userRouter.get("/users/current/watching", ensureAuth, userController.watching);
+userRouter.get("/users/current", ensureAuth, userController.show);
 
 export { userRouter };
